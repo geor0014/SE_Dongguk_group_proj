edc552c655a7b04cddc64ea5266a9443d96cd6cb
geor0014 <geor0014@hz.nl>
2024-05-31 15:14:52 +0900

    feat (tailwind) add tailwind to SendMessage and fix class names in NavBar and UploadImage

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -122,32 +122,3 @@ export default function NavBar() {
     </nav>
   );
 }
-
-// <nav class="relative bg-white shadow dark:bg-gray-800">
-//     <div class="container px-6 py-3 mx-auto md:flex">
-//         <div class="flex items-center justify-between">
-
-//             <!-- Mobile menu button -->
-//             <div class="flex lg:hidden">
-//                 <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-//                 </button>
-//             </div>
-//         </div>
-
-//         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-//         <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 md:translate-x-0 md:flex md:items-center md:justify-between">
-//             <div class="flex flex-col px-2 -mx-4 md:flex-row md:mx-10 md:py-0">
-//                 <a href="#" class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Home</a>
-//                 <a href="#" class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">About</a>
-//                 <a href="#" class="px-2.5 py-2 text-gray-700 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 md:mx-2">Contact</a>
-//             </div>
-
-//             <div class="relative mt-4 md:mt-0">
-//                 <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-//                 </span>
-
-//                 <input type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
-//             </div>
-//         </div>
-//     </div>
-// </nav>
--- frontend/src/components/UploadImage.js
+++ frontend/src/components/UploadImage.js
@@ -43,15 +43,15 @@ export default function UploadImage() {
     <>
       {user && (
         <section className="flex flex-col max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg md:flex-row md:h-100">
-          <div class="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
-            <div class="px-6 py-6 md:px-8 md:py-0">
-              <h2 class="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100">
+          <div className="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
+            <div className="px-6 py-6 md:px-8 md:py-0">
+              <h2 className="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100">
                 Upload Your{" "}
-                <span class="text-blue-600 dark:text-blue-400 md:text-blue-300">
+                <span className="text-blue-600 dark:text-blue-400 md:text-blue-300">
                   Images
                 </span>
               </h2>
-              <p class="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">
+              <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">
                 Upload and share images with the community so that other users
                 can see them and respond to them
               </p>
@@ -95,26 +95,4 @@ export default function UploadImage() {
       )}
     </>
   );
-}
-
-{
-  /* <section class="flex flex-col max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 md:flex-row md:h-48">
-    <div class="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
-        <div class="px-6 py-6 md:px-8 md:py-0">
-            <h2 class="text-lg font-bold text-gray-700 dark:text-white md:text-gray-100">Sign Up For <span class="text-blue-600 dark:text-blue-400 md:text-blue-300">Project</span> Updates</h2>
-
-            <p class="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur obcaecati odio</p>
-        </div>
-    </div>
-
-    <div class="flex items-center justify-center pb-6 md:py-0 md:w-1/2">
-        <form>
-            <div class="flex flex-col p-1.5 overflow-hidden border rounded-lg dark:border-gray-600 lg:flex-row dark:focus-within:border-blue-300 focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-                <input class="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none  focus:placeholder-transparent dark:focus:placeholder-transparent" type="text" name="email" placeholder="Enter your email" aria-label="Enter your email">
-
-                <button class="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">subscribe</button>
-            </div>
-        </form>
-    </div>
-</section> */
 }
